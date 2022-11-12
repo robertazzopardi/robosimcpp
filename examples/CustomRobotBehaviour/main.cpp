6211425280d8872593fc16cc689b76072055afed
robertazzopardi <robertazzopardi@icloud.com>
2022-11-12 00:47:04 +0000

    final refactoring and restructing to project and build system

--- examples/CustomRobotBehaviour/main.cpp
+++ examples/CustomRobotBehaviour/main.cpp
@@ -1,36 +1,20 @@
-/**
- * @file main.cpp
- * @author Robert Azzopardi-Yashi (robertazzopardi@icloud.com)
- * @brief
- * @version 0.1
- * @date 2021-07-05
- *
- * @copyright Copyright (c) 2021
- *
- * Compile This File With:
- * clang++ -o main main.cpp -I/usr/local/include/robosim
- * -L/usr/local/lib -lrobosim
- * ./main
- *
- */
-// clang++ -Wall -Werror -Wextra -std=c++20 -o main main.cpp
-// -I/usr/local/include/robosim -L/usr/local/lib -lrobosim; ./main
-
-#include "robosim.h"
+#include <robosim/robosim.h>
 #include <iostream>
-#include <random>
 
-class Robot : public robosim::robotmonitor::RobotMonitor {
-  public:
+class Robot : public robosim::robotmonitor::RobotMonitor
+{
+public:
     // Inherit constructor (essentially super the RobotMonitor constructor)
     Robot(bool verbose, colour::Colour colour)
         : robosim::robotmonitor::RobotMonitor(verbose, colour) {}
 
     // Override run, to implement the robots behaviour
-    void run(bool *running) {
+    void run(bool *running)
+    {
         std::cout << "Starting Robot: " << serialNumber << std::endl;
 
-        while (*running) {
+        while (*running)
+        {
             travel();
 
             rotate(90);
@@ -40,13 +24,15 @@ class Robot : public robosim::robotmonitor::RobotMonitor {
     }
 };
 
-int main(void) {
+int main(int argc, char **argv)
+{
+    // obvious error handling here
+    static const char *configFile = argv[1];
+
     robosim::envcontroller::makeRobots<Robot>(3, colour::OFF_BLACK);
 
-    robosim::envcontroller::EnvController("./defaultConfig.txt", 50);
+    robosim::envcontroller::EnvController(configFile, 50);
     // robosim::envcontroller::EnvController(10, 10, 50);
 
     robosim::envcontroller::startSimulation();
-
-    return 0;
 }
